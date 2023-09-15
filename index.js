a804885397c2fc66fad41fa98bca0f4bfa710f3b
aamirfarookh <aamirfarooqbhatt@gmail.com>
2023-09-15 22:55:29 +0530

    ok

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ $(document).ready(function(){
 
 
      var typed = new Typed (".panku2",{
-      strings:["Full-Stack Web-Developer", "Node-js Backend Devloper"],
+      strings:["Full-Stack Web-Developer", "Node-js Backend Developer"],
       typeSpeed:100,
       backSpeed:60,
       loop:true
